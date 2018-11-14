e9a53f9e8012c8fb5480b6662e7e3a7af9cddfb1
xiaohuaye <908347816@qq.com>
2018-11-15 02:18:16 +0800

    代码优化版-可加任意张数图片

--- seamlessCarousel/carousel.js
+++ seamlessCarousel/carousel.js
@@ -1,34 +1,33 @@
+// 初始化
 $('.imges>img:nth-child(1)').addClass('current')
-$('.imges>img:nth-child(2)').addClass('enter')
-$('.imges>img:nth-child(3)').addClass('leave')
+$('.imges>img:nth-child(1)').siblings().addClass('enter')
 
 var size = $('.imges').children().length
 var n = 0
 
 setInterval(()=>{
-	if (n%3 === 0) {
-		$('.imges>img:nth-child(1)').removeClass('leave current enter').addClass('leave')
-		.one('transitionend',()=>{
-			$('.imges>img:nth-child(1)').removeClass('leave').addClass('enter')
-		})
-		$('.imges>img:nth-child(2)').removeClass('leave current enter').addClass('current')
-		$('.imges>img:nth-child(3)').removeClass('leave current enter').addClass('enter')
-	}
-	if (n%3 === 1) {
-		$('.imges>img:nth-child(2)').removeClass('leave current enter').addClass('leave')
-		.one('transitionend',()=>{
-			$('.imges>img:nth-child(2)').removeClass('leave').addClass('enter')
-		})
-		$('.imges>img:nth-child(3)').removeClass('leave current enter').addClass('current')
-		$('.imges>img:nth-child(1)').removeClass('leave current enter').addClass('enter')
-	}
-	if (n%3 === 2) {
-		$('.imges>img:nth-child(3)').removeClass('leave current enter').addClass('leave')
-		.one('transitionend',()=>{
-			$('.imges>img:nth-child(3)').removeClass('leave').addClass('enter')
-		})
-		$('.imges>img:nth-child(1)').removeClass('leave current enter').addClass('current')
-		$('.imges>img:nth-child(2)').removeClass('leave current enter').addClass('enter')
-	}
+	let x = n%size
+		CurrentChangeLeave(x)
+		nextEnterChangeCurrentAndOthersChangeEnter(x,size)
 	n = n + 1
 },3000) 
+
+//函数声明
+function CurrentChangeLeave(y){
+	$('.imges>img:nth-child('+ (y+1) +')').removeClass('leave current enter').addClass('leave')
+	.one('transitionend',()=>{
+		$('.imges>img:nth-child('+ (y+1) +')')
+		.removeClass('leave').addClass('enter')
+	})
+}
+function nextEnterChangeCurrentAndOthersChangeEnter(y,n){
+	if (y+1<n) {
+			$('.imges>img:nth-child('+ (y+2) +')').removeClass('leave current enter').addClass('current')
+			$('.imges>img:nth-child('+ (y+2) +')').siblings().not('.imges>img:nth-child('+ (y+1) +')')
+			.removeClass('leave current enter').addClass('enter')
+		}else{
+			$('.imges>img:nth-child(1)').removeClass('leave current enter').addClass('current')
+			$('.imges>img:nth-child(1)').siblings().not('.imges>img:nth-child('+ (y+1) +')')
+			.removeClass('leave current enter').addClass('enter')
+		}
+}
