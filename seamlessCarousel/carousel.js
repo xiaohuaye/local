4c7512e90b2a33f72cd92402451e9e8222e1954a
xiaohuaye <908347816@qq.com>
2018-11-15 14:49:58 +0800

    无缝轮播第一版

--- seamlessCarousel/carousel.js
+++ seamlessCarousel/carousel.js
@@ -5,16 +5,22 @@ $('.imges>img:nth-child(1)').siblings().addClass('enter')
 var size = $('.imges').children().length
 var n = 0
 var timer = setTimer()
-
-setInterval(()=>{
-	let x = n%size
-		CurrentChangeLeave(x)
-		nextEnterChangeCurrentAndOthersChangeEnter(x,size)
-	let index = $('.current').index()
-		redSpot(index)
-	n = n + 1
-},3000) 
-
+$('.window').on('mouseenter',function(){
+	clearInterval(timer)
+})
+$('.window').on('mouseleave',function(){
+  timer = setTimer()
+})
+$('.localPic>span').on('mouseenter',function(x){
+	let index1 = $(x.currentTarget).index()
+	clearInterval(timer)
+	redSpot(index1)
+	CurrentChangeLeave(index1-1)
+	nextEnterChangeCurrentAndOthersChangeEnter(index1-1,size)
+})
+$('.localPic>span').on('mouseleave',function(){
+	timer = setTimer()
+})
 //函数声明
 function CurrentChangeLeave(y){
 	$('.imges>img:nth-child('+ (y+1) +')').removeClass('leave current enter').addClass('leave')
@@ -37,4 +43,15 @@ function nextEnterChangeCurrentAndOthersChangeEnter(y,n){
 function redSpot(y){
 	$('.localPic>span').removeClass('red')
 	$('.localPic>span:nth-child('+ (y+1) +')').addClass('red')
+}
+
+function setTimer(){
+	return setInterval(()=>{
+	let x = n%size
+		CurrentChangeLeave(x)
+		nextEnterChangeCurrentAndOthersChangeEnter(x,size)
+	let index = $('.current').index()
+		redSpot(index)
+	n = n + 1
+	},3000) 
 }
